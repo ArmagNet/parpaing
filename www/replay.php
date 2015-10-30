417197d6877729cf7d9872e6e18a81b7ccde4672
farlistener <contact@levieuxcedric.com>
2015-10-30 13:33:31 +0100

    start the tv pages

---
+++ www/replay.php
@@ -0,0 +1,46 @@
+<?php /*
+	Copyright 2014-2015 Cédric Levieux, Jérémy Collot, ArmagNet
+
+	This file is part of Parpaing.
+
+    Parpaing is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Parpaing is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Parpaing.  If not, see <http://www.gnu.org/licenses/>.
+*/
+include_once("header.php");
+?>
+<div class="container theme-showcase" role="main">
+	<ol class="breadcrumb">
+		<li><?php echo lang("breadcrumb_index"); ?></li>
+		<li class="active"><?php echo lang("breadcrumb_replay"); ?></li>
+	</ol>
+
+	<div class="col-md-12">
+
+		<div class="col-md-4 channel-tv">
+			<a href="http://pluzz.francetv.fr/" target="_blank"><img src="images/logos/logo_francepluzz.png" /></a>
+		</div>
+
+		<div class="col-md-4 channel-tv">
+			<a href="http://www.6play.fr/" target="_blank"><img src="images/logos/logo_6play.png" /></a>
+		</div>
+
+	</div>
+</div>
+
+<div class="lastDiv"></div>
+
+<?php include("footer.php");?>
+<script>
+</script>
+</body>
+</html>
--- www/tv.php
+++ www/tv.php
@@ -26,7 +26,66 @@ include_once("header.php");
 
 	<div class="col-md-12">
 
-	SOON !
+		<!-- FT Group -->
+
+		<div class="col-md-4 channel-tv">
+			<a href="http://www.france2.fr/direct" target="_blank"><img src="images/logos/logo_france2.png" /></a>
+		</div>
+		<div class="col-md-4 channel-tv">
+			<a href="http://www.france3.fr/direct" target="_blank"><img src="images/logos/logo_france3.png" /></a>
+		</div>
+		<div class="col-md-4 channel-tv">
+			<a href="http://www.france4.fr/direct" target="_blank"><img src="images/logos/logo_france4.png" /></a>
+		</div>
+		<div class="col-md-4 channel-tv">
+			<a href="http://www.france5.fr/direct" target="_blank"><img src="images/logos/logo_france5.png" /></a>
+		</div>
+		<div class="col-md-4 channel-tv">
+			<a href="http://www.franceo.fr/direct" target="_blank"><img src="images/logos/logo_franceo.png" /></a>
+		</div>
+
+		<div class="col-md-4 channel-tv">
+			<a href="http://www.6play.fr/m6/direct#/m6/direct" target="_blank"><img src="images/logos/logo_m6.png" /></a>
+		</div>
+
+		<!-- M6 Group -->
+
+		<div class="col-md-4 channel-tv">
+			<a href="http://www.6play.fr/w9/direct#/w9/direct" target="_blank"><img src="images/logos/logo_w9.png" /></a>
+		</div>
+
+		<div class="col-md-4 channel-tv">
+			<a href="http://www.6play.fr/6ter/direct#/6ter/direct" target="_blank"><img src="images/logos/logo_6ter.png" /></a>
+		</div>
+
+		<!-- BFM Group -->
+
+		<div class="col-md-4 channel-tv">
+			<a href="http://www.bfmtv.com/mediaplayer/live-video/" target="_blank"><img src="images/logos/logo_bfmtv.png" /></a>
+		</div>
+
+		<div class="col-md-4 channel-tv">
+			<a href="http://bfmbusiness.bfmtv.com/mediaplayer/live-video/" target="_blank"><img src="images/logos/logo_bfmbusiness.png" /></a>
+		</div>
+
+		<div class="col-md-4 channel-tv audio">
+			<a href="http://rmc.bfmtv.com/mediaplayer/live-audio/" target="_blank"><img src="images/logos/logo_rmc.png" /></a>
+		</div>
+
+		<div class="col-md-4 channel-tv">
+			<a href="http://rmcdecouverte.bfmtv.com/mediaplayer-direct/" target="_blank"><img src="images/logos/logo_rmcdecouverte.png" /></a>
+		</div>
+
+		<div class="col-md-4 channel-tv">
+			<a href="http://rmcsport.bfmtv.com/mediaplayer/" target="_blank"><img src="images/logos/logo_rmcsport.png" /></a>
+		</div>
+
+		<div class="col-md-4 channel-tv">
+			<a href="http://www.01net.com/mediaplayer/" target="_blank"><img src="images/logos/logo_01net.png" /></a>
+		</div>
+
+
+
 
 	</div>
 </div>
