e8873e477ae2bf2157ef4fc834ea9ae1522efaf6
farlistener <contact@levieuxcedric.com>
2016-11-26 12:37:47 +0100

    0.9.0.4.7
    Add store in github

---
+++ store/0.9.0.4.7/language/fr/main.php
@@ -0,0 +1,162 @@
+<?php /*
+	Copyright 2014-2015 Cédric Levieux, Jérémy Collot, ArmagNet
+
+	This file is part of Parpaing.
+
+    Parpaing is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Parpaing is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Parpaing.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+$lang["date_format"] = "d/m/Y";
+$lang["time_format"] = "H:i";
+$lang["datetime_format"] = "le {date} à {time}";
+
+$lang["common_validate"] = "Valider";
+$lang["common_delete"] = "Supprimer";
+$lang["common_activate"] = "Activer";
+$lang["common_deactivate"] = "Désactiver";
+$lang["common_add"] = "Ajouter";
+$lang["common_modify"] = "Modifier";
+$lang["common_reset"] = "Reset";
+$lang["common_connect"] = "Connecter";
+$lang["common_order"] = "Commander";
+$lang["common_update"] = "Mettre à jour";
+$lang["common_authenticate"] = "Identifier";
+$lang["common_change"] = "Changer";
+$lang["common_send"] = "Envoyer";
+$lang["common_cancel"] = "Annuler";
+$lang["common_close"] = "Fermer";
+
+$lang["language_fr"] = "Français";
+$lang["language_en"] = "Anglais";
+$lang["language_de"] = "Allemand";
+
+$lang["parpaing_title"] = "Parpaing - Un mini routeur VPN";
+
+$lang["menu_language"] = "Langue : {language}";
+$lang["menu_status"] = "Statut";
+$lang["menu_vpn"] = "VPN";
+$lang["menu_tv"] = "TV";
+$lang["menu_replay"] = "Replay";
+$lang["menu_telephone"] = "Téléphone";
+$lang["menu_wifi"] = "Wifi";
+$lang["menu_explorer"] = "Explorateur";
+$lang["menu_bittorrent"] = "BitTorrent";
+$lang["menu_upnp"] = "UPnP";
+$lang["menu_upgrader"] = "Mise à jour";
+$lang["menu_logout"] = "Se déconnecter";
+
+$lang["upgrader_actualVersion"] = "Version actuelle";
+$lang["upgrader_newVersion"] = "Version disponible";
+$lang["upgrader_version_version"] = "Version";
+$lang["upgrader_version_description"] = "Description";
+$lang["upgrader_startUpgrade_button"] = "Faire la mise à jour";
+$lang["upgrader_rebootInterface_button"] = "Relancer l'interface";
+
+$lang["login_title"] = "Identifiez vous";
+$lang["login_loginInput"] = "Identifiant";
+$lang["login_passwordInput"] = "Mot de passe";
+$lang["login_button"] = "Me connecter";
+$lang["login_rememberMe"] = "Se souvenir de moi";
+$lang["register_link"] = "ou m'enregistrer";
+$lang["forgotten_link"] = "j'ai oublié mon mot de passe";
+
+$lang["breadcrumb_index"] = "Accueil";
+$lang["breadcrumb_vpn"] = "VPN";
+$lang["breadcrumb_tv"] = "TV";
+$lang["breadcrumb_replay"] = "Replay";
+$lang["breadcrumb_telephone"] = "Telephone";
+$lang["breadcrumb_wifi"] = "Wifi";
+$lang["breadcrumb_explorer"] = "Explorateur";
+$lang["breadcrumb_bittorrent"] = "BitTorrent";
+$lang["breadcrumb_upnp"] = "Universal Plug and Play";
+$lang["breadcrumb_networks"] = "Réseaux";
+$lang["breadcrumb_piraxplorer"] = "Piratebox";
+$lang["breadcrumb_upgrader"] = "Mise à jour";
+$lang["breadcrumb_about"] = "À Propos";
+
+$lang["index_guide"] = "Parpaing est l'interface de gestion simple de votre mini routeur VPN.";
+$lang["index_authentication_panel"] = "Identification";
+$lang["index_authentication_password"] = "Mot de passe";
+$lang["index_authentication_password_placeholder"] = "mot de passe du Parpaing";
+$lang["index_authentication_newpassword"] = "Nouveau mot de passe";
+$lang["index_authentication_newpassword_placeholder"] = "nouveau mot de passe du Parpaing";
+$lang["index_authentication_confirm"] = "Confirmation";
+$lang["index_authentication_confirm_placeholder"] = "confirmation du mot de passe du Parpaing";
+
+$lang["index_status_panel"] = "Statut";
+$lang["index_memory_legend"] = "Mémoire";
+$lang["index_memory_total_label"] = "Memoire totale :";
+$lang["index_memory_free_label"] = "Memoire disponible :";
+$lang["index_swap_total_label"] = "Swap total :";
+$lang["index_swap_free_label"] = "Swap disponible :";
+$lang["index_swap_used_label"] = "Swap utilisé :";
+$lang["index_disk_legend"] = "Disque";
+$lang["index_disk_total_label"] = "Taille totale :";
+$lang["index_disk_free_label"] = "Taille disponible :";
+$lang["index_cpu_legend"] = "CPU";
+$lang["index_cpu_load_label"] = "Load :";
+$lang["index_cpu_nb_label"] = "Nombre de CPU :";
+$lang["index_cpu_0_label"] = "CPU :";
+$lang["index_cpu_x_label"] = "CPU {x} :";
+$lang["index_network_legend"] = "Réseau";
+$lang["index_network_download_label"] = "Download :";
+$lang["index_network_upload_label"] = "Upload :";
+$lang["index_network_no_ip"] = "Pas d'IP";
+
+$lang["bittorrent_url_label"] = "URL / Magnet :";
+$lang["bittorrent_question_remove"] = "Voulez vous retirer le torrent ?";
+$lang["bittorrent_question_trash"] = "Voulez vous retirer le torrent ainsi que les fichiers associés ?";
+
+$lang["explorer_prompt_createFolder"] = "Nom du nouveau répertoire";
+$lang["explorer_prompt_addFile"] = "Fichiers à transférer";
+$lang["explorer_tooltip_download"] = "Téléchargement direct";
+$lang["explorer_tooltip_list"] = "Liste";
+$lang["explorer_tooltip_icons"] = "Icônes";
+$lang["explorer_tooltip_addFiles"] = "Ajout de fichiers";
+$lang["explorer_tooltip_createFolder"] = "Création de répertoire";
+
+$lang["networks_network_safe"] = "Réseau sécurisé";
+$lang["networks_network_unsafe"] = "Réseau non sécurisé";
+$lang["networks_mac_type_static"] = "Matériel statique";
+$lang["networks_mac_type_mobile"] = "Matériel mobile";
+$lang["networks_mac_type_network"] = "Matériel réseau";
+$lang["networks_mac_type_desktop"] = "Ordinateur";
+$lang["networks_mac_type_tv"] = "Téléviseur";
+$lang["networks_mac_type_print"] = "Imprimante";
+$lang["networks_mac_type_laptop"] = "Portable";
+$lang["networks_mac_type_phone"] = "Mobile";
+$lang["networks_mac_type_tablet"] = "Tablette";
+$lang["networks_mac_type_switch"] = "Réseau";
+$lang["networks_mac_type_server"] = "Serveur";
+
+$lang["piratebox_access"] = "Ou accès à la <a href='piraxplorer.php'>piratebox</a>";
+
+$lang["upnp_interface"] = "Réseau sur lequel le service est visible";
+
+// Rates
+
+$lang["memory_rate"] = "Mémoire";
+$lang["disk_rate"] = "Disque";
+$lang["cpu_rate"] = "CPU";
+$lang["download_rate"] = "Download";
+$lang["upload_rate"] = "Upload";
+
+$lang["defaultPasswordAlert"] = "Mot de passe par défaut, à changer";
+$lang["notSameNewPasswordAlert"] = "Les nouveaux mots de passe indiqués ne sont pas identiques";
+$lang["badPasswordAlert"] = "Mauvais mot de passe";
+
+$lang["about_footer"] = "À Propos";
+$lang["advanced_intertace_footer"] = "Interface avancée";
+$lang["parpaing_footer"] = "Parpaing est une application fournie par <a href=\"https://www.armagnet.fr\" target=\"_blank\">ArmagNet</a>";
+?>
